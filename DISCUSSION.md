4f12ac851489321eccaeeb092697d6c9d82b4249
Charles Simms <cssimms@gmail.com>
2025-11-01 17:22:09 -0700

    Add some basic components, render a bit nicer with some pre-built tailwind classses.

--- DISCUSSION.md
+++ DISCUSSION.md
@@ -13,7 +13,8 @@ This will cause a hydration error.
 
 ~ wrap the <th> in their own <tr>
 
-- [ ] js console errors
+- [x] js console errors
+  - some nextjs stuff but also just html and react easy things to fix
 
 ```
 Warning: Each child in a list should have a unique "key" prop.
@@ -22,12 +23,13 @@ Warning: Each child in a list should have a unique "key" prop.
 this got me down a whole typing rabbit hole, (but a good one!) getting the types from the backend and using them on the Page.
 "specialties" is spelled wrong and is also an unknown json blob. changing to an array of strings, bc that seems reasonable.
 
-- some nextjs stuff but also just html and react easy things to fix
-
 - [ ] front end scaling
 
-  - are we fetchign all records?
+  - are we fetching all records?
   - is there pagination?
 
 - [ ] searching doesn't work at all
+
 - [ ] reset search bar doesn't clear input value
+
+- [ ] basic styling... it's hard to look at honestly
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
     setFilteredAdvocates(filteredAdvocates);
   };
 
-  const onClick = () => {
+  const resetSearch = () => {
     console.log(advocates);
     setFilteredAdvocates(advocates);
   };
@@ -48,36 +48,23 @@ export default function Home() {
       <h1>Solace Advocates</h1>
       <br />
       <br />
-      <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term"></span>
-        </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
+      <div className="flex items-center border border-gray-300 rounded-lg p-2">
+        <input
+          className="flex-grow outline-none bg-transparent px-2"
+          style={{ border: "1px solid black" }}
+          onChange={onChange}
+          placeholder="Type here to search ... "
+        />
+        <button
+          className="ml-2 p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none"
+          onClick={resetSearch}
+        >
+          Reset Search
+        </button>
       </div>
       <br />
       <br />
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-            <th>Degree</th>
-            <th>Specialties</th>
-            <th>Years of Experience</th>
-            <th>Phone Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredAdvocates.map((advocate) => {
-            return (
-              <AdvocateRow key={advocate.id} advocate={advocate}></AdvocateRow>
-            );
-          })}
-        </tbody>
-      </table>
+      <AdvocateResults advocates={filteredAdvocates} />
     </main>
   );
 }
@@ -86,20 +73,99 @@ interface AdvocateRowProps {
   advocate: Advocate;
 }
 
-function AdvocateRow({ advocate }: AdvocateRowProps) {
+interface AdvocateResultsProps {
+  advocates: Advocate[];
+}
+
+function AdvocateResults({ advocates }: AdvocateResultsProps) {
   return (
-    <tr key={advocate.id}>
-      <td>{advocate.firstName}</td>
-      <td>{advocate.lastName}</td>
-      <td>{advocate.city}</td>
-      <td>{advocate.degree}</td>
-      <td>
-        {advocate.specialties.map((s, index) => (
-          <div key={`${advocate.id}${index}`}>{s}</div>
-        ))}
-      </td>
-      <td>{advocate.yearsOfExperience}</td>
-      <td>{advocate.phoneNumber}</td>
-    </tr>
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      {advocates.map((advocate) => (
+        <AdvocateRow key={advocate.id} advocate={advocate}></AdvocateRow>
+      ))}
+    </div>
+  );
+}
+
+function AdvocateRow({ advocate }: AdvocateRowProps) {
+  // Generic card styles copied from tailwind documentation
+  // https://tailwindcss.com/docs/styling-with-utility-classes
+  // Could use a lot of work! But wanted to just get started with something in place of the html table
+  return (
+    <div className="flex gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+      <AdvocateIcon />
+      <div className="space-y-2 text-center sm:text-left">
+        <AdvocateName advocate={advocate} />
+        <div className="space-y-0.5">
+          <p className="font-medium text-gray-500">{advocate.city}</p>
+          <span className="font-medium text-gray-500">
+            In the industry for {advocate.yearsOfExperience} years
+          </span>
+          <AdvocatePhoneNumber advocate={advocate} />
+        </div>
+        <AdvocateSpecialties advocate={advocate} />
+        <button className="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
+          Message (Or other call to action)
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function AdvocateIcon() {
+  // TODO - thought a placeholder svg would be fun, but I was getting strange sizing on the page.
+  // Left as a stretch goal, if we wanted to try and render something here.
+  // <svg className="w-6 h-6 text-gray-500"
+  //   fill="none"
+  //   stroke="currentColor"
+  //   viewBox="0 0 24 24"
+  //   xmlns="http://www.w3.org/2000/svg"
+  // >
+  //   <path
+  //     stroke-linecap="round"
+  //     stroke-linejoin="round"
+  //     stroke-width="2"
+  //     d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+  //   ></path>
+  // </svg>
+  return null;
+}
+
+function AdvocateName({ advocate }: { advocate: Advocate }) {
+  return (
+    <>
+      <p className="text-lg font-semibold text-black">
+        {advocate.firstName} {advocate.lastName},
+        <span className="font-medium text-gray-500"> {advocate.degree}</span>
+      </p>
+    </>
+  );
+}
+
+function AdvocatePhoneNumber({ advocate }: { advocate: Advocate }) {
+  return <p className="font-medium text-gray-500">{advocate.phoneNumber}</p>;
+}
+
+function AdvocateSpecialties({ advocate }: { advocate: Advocate }) {
+  // TODO - different effort to re-work specialities
+  // return (
+  //   <div>
+  //     <p className="font-medium text-gray-700">Specialties:</p>
+  //     <div className="flex">
+  //       {advocate.specialties.map((specialty, index) => (
+  //         <span
+  //           key={index}
+  //           className="my-2 inline-block rounded-full bg-neutral-200 px-6 pb-2 pt-2.5 text-xs font-small leading-normal  shadow-md transition duration-150 ease-in-out hover:bg-neutral-300 hover:shadow-lg focus:bg-neutral-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-neutral-400 active:shadow-lg dark:bg-neutral-600 dark:text-neutral-200 dark:hover:bg-neutral-500 dark:focus:bg-neutral-500 dark:active:bg-neutral-400"
+  //         >
+  //           {specialty}
+  //         </span>
+  //       ))}
+  //     </div>
+  //   </div>
+
+  return (
+    <span className="text-xs text-gray-700">
+      Specialties: {advocate.specialties.join(", ")}
+    </span>
   );
 }
